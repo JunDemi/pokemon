e5e3cf55333a1576865e8554bd4ef1cba9b37a89
JunDemi <jungwook3176@gmail.com>
2023-11-11 14:10:58 +0900

    Feat: 이벤트 핸들링 연습

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="띠부띠부씰"/>
   </div>
 </template>
 
